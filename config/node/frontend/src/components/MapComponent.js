680c22d33f250e866492ea9cbbb3c4f27a0c1498
Mateusz <u56172@student.wat.edu.pl>
2026-01-14 12:24:35 +0100

    feat: added new tile map

--- config/node/frontend/src/components/MapComponent.js
+++ config/node/frontend/src/components/MapComponent.js
@@ -5,6 +5,8 @@ import TileLayer from "ol/layer/Tile";
 import View from "ol/View";
 import {useGeographic} from 'ol/proj';
 import 'ol/ol.css';
+import {TileWMS} from "ol/source";
+
 function MapComponent(props) {
 
     const mapRef = useRef(null);
@@ -12,17 +14,35 @@ function MapComponent(props) {
     useEffect(() => {
         const map = new Map({
             target: mapRef.current,
-            layers:[
+            layers: [
                 new TileLayer({
                     source: new OSM(),
+                }),
+                // http://localhost:9000/geoserver/ne/wms?service=WMS&version=1.1.0&request=GetMap&layers=ne%3Acountries&bbox=-180.0%2C-90.0%2C180.0%2C83.64513&width=768&height=370&srs=EPSG%3A4326&styles=&format=application/openlayers
+                new TileLayer({
+                    source: new TileWMS({
+                      url: 'http://localhost:9000/geoserver/ne/wms?',
+                      params: {'LAYERS': 'ne:countries', 'TILED': true},
+                      serverType: 'geoserver',
+                      transition: 0,
+                    }),
+                    //http://localhost:9000/geoserver/wms?service=WMS&version=1.1.0&request=GetMap&layers=land_cover&bbox=534812.2907552869%2C449157.5986658251%2C578052.8563778237%2C494770.6695298003&width=728&height=768&srs=EPSG%3A2180&styles=&format=application/openlayers
+                }),
+                new TileLayer({
+                    source: new TileWMS({
+                        url: 'http://localhost:9000/geoserver/wms?',
+                        params: {'LAYERS': 'land_cover', 'TILED': true},
+                        serverType: 'geoserver',
+                        transition: 0,
+                    })
                 })
             ],
             view: new View({
-            center: [21, 52],
-            zoom: 6,
-          })
+                center: [21, 52],
+                zoom: 6,
+            })
         });
-        return ()=> map.setTarget(null)
+        return () => map.setTarget(null)
     }, [])
     return (
         <div className='mapComponent' ref={mapRef}>
